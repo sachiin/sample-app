661fcabd2bb5e593e1941c11f9bbce810d7faaf0
sachiin <thesachiinn@gmail.com>
2013-07-21 16:54:30 +0530

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -82,7 +82,26 @@ describe "authorization" do
           before { visit users_path }
           it { should have_title('Sign in') }
         end  # end index
+       describe "visiting the following page" do
+          before { visit following_user_path(user)}
+          it { should have_title('Sign in') }
+       end
+         describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
       end  #end user controller
+     describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end #end relationship controller
      end  # non signin
 describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
